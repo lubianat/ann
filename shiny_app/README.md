1ad485b7a05f2322c318e85ddbe64a195afe08b9
lubianat <tiago.lubiana.alves@usp.br>
2020-08-29 19:55:39 -0300

    Add simple shiny app for abstracts

---
+++ shiny_app/README.md
@@ -0,0 +1,9 @@
+# Shiny Apps
+
+Shiny Apps are web apps which can be written in R, with support of the RStudio IDE.
+
+They are nice for prototyping, as they can be integrated with a great number of 
+
+## Display Abstracts
+
+A very simple interface that gets abstracts from Europe PMC using the PMID and displays to user. 
---
+++ shiny_app/abstract_display/app.R
@@ -0,0 +1,27 @@
+# Simple application that displays abstracts from EuropePMC
+
+
+library(shiny)
+library(europepmc)
+
+get_abstract <- function(pmid){
+    
+    api_result <- epmc_details(pmid)
+    api_result[["basic"]][["abstractText"]]
+    
+}
+
+ui <- fluidPage(
+    textInput(inputId = "pmid",value = "32180547", "PMID"),
+    h2("Abstract:"), 
+    textOutput("abstract"),
+)
+
+server <- function(input, output) {
+    abstract_text<-  eventReactive(input$pmid, {get_abstract(input$pmid)
+    })
+    
+    output$abstract <- renderText(abstract_text())
+}
+
+shinyApp(ui = ui, server = server)
