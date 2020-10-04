dc5b78122fa294b674e9db339291a9c2f952086b
jvfe <jvfecav@gmail.com>
2020-10-04 15:11:58 -0300

    chore: Turning the shiny app into a R project
    Turns the R Shiny prototype into an R project, together with an isolated
    Renv, to make it easier for other people to run it. Also applies linting
    (from lintr) to conform the code style to the tidyverse style guide.
    
    * Addresses issue #29

--- prototypes/shiny_app/abstract_display/app.R
+++ prototypes/shiny_app/abstract_display/app.R
@@ -1,26 +1,26 @@
 # Simple application that displays abstracts from EuropePMC
 
-
 library(shiny)
 library(europepmc)
 
-get_abstract <- function(pmid){
-    
+get_abstract <- function(pmid) {
+
     api_result <- epmc_details(pmid)
     api_result[["basic"]][["abstractText"]]
-    
+
 }
 
 ui <- fluidPage(
-    textInput(inputId = "pmid",value = "32180547", "PMID"),
-    h2("Abstract:"), 
+    textInput(inputId = "pmid", value = "32180547", "PMID"),
+    h2("Abstract:"),
     textOutput("abstract"),
 )
 
 server <- function(input, output) {
-    abstract_text<-  eventReactive(input$pmid, {get_abstract(input$pmid)
-    })
-    
+    abstract_text <-  eventReactive(input$pmid, {
+        get_abstract(input$pmid)
+})
+
     output$abstract <- renderText(abstract_text())
 }
 
